776fe09cd88e8a73854b95bdbfb4e22b26e8c304
Ilija Vukotic <ivukotic@cern.ch>
2015-10-22 11:54:47 -0500

    cl

--- TestInsert.py
+++ TestInsert.py
@@ -5,8 +5,6 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch([{'host':'aianalytics01.cern.ch', 'port':9200}])
 
-# "jobid": 9,"step": "RAWtoESD","component":"InDetSiSpTrackFinder","domain": "InnerDetector","stage": "evt","n": 99,"cpu"  : 7432,"vmem" : 0,"malloc" : 18870
-
 doc = {
     'jobid': 9,
     'step': 'RAWtoESD',
